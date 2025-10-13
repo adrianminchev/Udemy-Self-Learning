e8dd550d9a341c762a8bcecee8b5cd2ddf7e221c
adrianminchev <minchev.adrian13@gmail.com>
2025-10-13 21:22:11 +0300

    Validate user input fields and set show error modal

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/project-management-app/src/App.jsx
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/project-management-app/src/App.jsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddingProject() {
+    setProjectState((prevState) => {
+      return { ...prevState, selectedProjectId: null };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = (
+      <NoProjectSelected onStartAddProject={handleStartAddingProject} />
+    );
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <Sidebar />
-      <NewProject />
+      <Sidebar
+        onStartAddProject={handleStartAddingProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
--- React - The Complete Guide 2025 (incl. Next.js, Redux)/project-management-app/src/components/Input.jsx
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/project-management-app/src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
   const classesStyle =
     "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
@@ -8,10 +10,12 @@ export default function Input({ label, textarea, ...props }) {
         {label}
       </label>
       {textarea ? (
-        <textarea className={classesStyle} {...props} />
+        <textarea ref={ref} className={classesStyle} {...props} />
       ) : (
-        <input className={classesStyle} {...props} />
+        <input ref={ref} className={classesStyle} {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
