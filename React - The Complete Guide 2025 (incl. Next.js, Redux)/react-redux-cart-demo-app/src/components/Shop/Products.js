4bc6ef2348d661dd77ce5bb220d062bc63b6ad99
adrianminchev <minchev.adrian13@gmail.com>
2025-11-15 19:42:16 +0200

    Dispatch actions from inside of the components

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/react-redux-cart-demo-app/src/components/Shop/Products.js
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/react-redux-cart-demo-app/src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
-const Products = (props) => {
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "First Special Book Edition",
+    description: "The first book that I have purchased",
+  },
+  {
+    id: "p2",
+    price: 7,
+    title: "Second Very Special Book Edition",
+    description: "The second book that I have purchased",
+  },
+];
+
+const Products = () => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
