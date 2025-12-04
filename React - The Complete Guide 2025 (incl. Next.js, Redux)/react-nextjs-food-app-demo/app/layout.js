b06ca4e162f684e9bb0f36c18854c9b658f87c7d
adrianminchev <minchev.adrian13@gmail.com>
2025-12-04 16:28:12 +0200

    Add custom component to a layout and add styles

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/react-nextjs-food-app-demo/app/layout.js
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/react-nextjs-food-app-demo/app/layout.js
@@ -1,14 +1,19 @@
-import './globals.css'
+import MainHeader from "./components/main-header/main-header";
+import "./globals.css";
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+  title: "Food App Demo",
+  description:
+    "Food related demo application that lists meals, that are shared by the food-loving community.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainHeader />
+        {children}
+      </body>
     </html>
   );
 }
