dc73245cf80c4d5e2d9e12a1d4248b29082387a4
adrianminchev <minchev.adrian13@gmail.com>
2025-12-07 21:23:19 +0200

    Set use of client components and output meals data

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/react-nextjs-food-app-demo/app/components/main-header/main-header.js
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/react-nextjs-food-app-demo/app/components/main-header/main-header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import NavLink from "./nav-link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
@@ -16,10 +17,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Food Community</Link>
+              <NavLink href="/community">Food Community</NavLink>
             </li>
           </ul>
         </nav>
