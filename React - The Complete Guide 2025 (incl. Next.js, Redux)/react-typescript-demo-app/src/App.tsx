7fef8613274234c1f117d8f20033e39fd0e739c6
adrianminchev <minchev.adrian13@gmail.com>
2025-12-17 19:58:03 +0200

    Add basic todo app structure with refs

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/react-typescript-demo-app/src/App.tsx
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/react-typescript-demo-app/src/App.tsx
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TS")];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo />
+      <Todos items={todos} />
     </div>
   );
 }
