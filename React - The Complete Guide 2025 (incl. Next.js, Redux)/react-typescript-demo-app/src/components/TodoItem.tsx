8f905719e2a3916d81bc3fbcbd1a5b3770c03677
adrianminchev <minchev.adrian13@gmail.com>
2025-12-17 20:17:51 +0200

    Add on click remove todo item

--- React - The Complete Guide 2025 (incl. Next.js, Redux)/react-typescript-demo-app/src/components/TodoItem.tsx
+++ React - The Complete Guide 2025 (incl. Next.js, Redux)/react-typescript-demo-app/src/components/TodoItem.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li>{props.text}</li>;
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (
+  props
+) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      <b>{props.text}</b> (Remove Todo item by clicking on it, only if it is
+      fulfilled!)
+    </li>
+  );
 };
 export default TodoItem;
